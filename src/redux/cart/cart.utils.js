2f1f503ebeeae36e8de1a0ee5e5d6814abd160c9
Nick-Kh <many_sided@hotmail.com>
2020-03-06 19:03:11 +0200

    added add-item and remove-item functionality to chekcout component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === itemToRemove.id)
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== itemToRemove.id)
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === itemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity -1}
+        : cartItem    
+    )
+}
